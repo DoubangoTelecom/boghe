a5a91cafd9531cbc7bb61272fbb76885095f0672
bossiel@yahoo.fr <bossiel@yahoo.fr@7f7133e8-ff9e-399d-0e3e-748f238f2c0b>
2012-09-05 06:46:33 +0000

    Align with Doubango 740

--- branches/2.0/boghe/boghe-app/Properties/AssemblyInfo.cs
+++ branches/2.0/boghe/boghe-app/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Doubango Telecom")]
 [assembly: AssemblyProduct("boghe-app")]
-[assembly: AssemblyCopyright("Copyright ©  2010 Doubango")]
+[assembly: AssemblyCopyright("Copyright ©  2010-2012 Doubango")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.104.715")]
-[assembly: AssemblyFileVersion("2.0.104.715")]
+[assembly: AssemblyVersion("2.0.106.731")]
+[assembly: AssemblyFileVersion("2.0.106.731")]
