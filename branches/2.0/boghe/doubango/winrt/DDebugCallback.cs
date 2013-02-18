56791635ab0c557a7305bc48481efa3e3bea09d5
bossiel@yahoo.fr <bossiel@yahoo.fr@7f7133e8-ff9e-399d-0e3e-748f238f2c0b>
2013-02-18 20:19:21 +0000

    Add Doubango-rt project to the svn repository.

---
+++ branches/2.0/boghe/doubango/winrt/DDebugCallback.cs
@@ -0,0 +1,60 @@
+﻿/*Copyright (C) 2013 Doubango Telecom <http://www.doubango.org>
+*	
+* This file is part of Open Source Doubango Framework.
+*
+* DOUBANGO is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*	
+* DOUBANGO is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*	
+* You should have received a copy of the GNU General Public License
+* along with DOUBANGO.
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using doubango_rt.BackEnd;
+
+namespace org.doubango.tinyWRAP
+{
+    public class DDebugCallback : rtIDDebugCallback
+    {
+        readonly rtDDebugCallback m_rtDDebugCallback;
+
+        public DDebugCallback()
+        {
+#if WINDOWS_PHONE
+            m_rtDDebugCallback = org.doubango.WindowsPhone.BackgroundProcessController.Instance.rtDDebugCallbackNew(this);
+#else
+            m_rtDDebugCallback = new rtDDebugCallback(this);
+#endif
+        }
+
+        public virtual int OnDebugError(string message)
+        {
+            return 0;
+        }
+
+        public virtual int OnDebugFatal(string message)
+        {
+            return 0;
+        }
+
+        public virtual int OnDebugInfo(string message)
+        {
+            return 0;
+        }
+
+        public virtual int OnDebugWarn(string message)
+        {
+            return 0;
+        }
+    }
+}
---
+++ branches/2.0/boghe/doubango/winrt/SipCallback.cs
@@ -0,0 +1,87 @@
+﻿/*Copyright (C) 2013 Doubango Telecom <http://www.doubango.org>
+*	
+* This file is part of Open Source Doubango Framework.
+*
+* DOUBANGO is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*	
+* DOUBANGO is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*	
+* You should have received a copy of the GNU General Public License
+* along with DOUBANGO.
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using doubango_rt.BackEnd;
+
+using DialogEvent = doubango_rt.BackEnd.rtDialogEvent;
+
+namespace org.doubango.tinyWRAP
+{
+    public class SipCallback : rtISipCallback
+    {
+        readonly rtSipCallback m_rtSipCallback;
+
+        public SipCallback()
+        {
+#if WINDOWS_PHONE
+            m_rtSipCallback = org.doubango.WindowsPhone.BackgroundProcessController.Instance.rtSipCallbackNew(this);
+#else
+            m_rtSipCallback = new rtSipCallback(this);
+#endif
+        }
+
+        public virtual int OnDialogEvent(DialogEvent e)
+        {
+            return 0;
+        }
+
+        public virtual int OnInfoEvent(rtInfoEvent e)
+        {
+            return 0;
+        }
+
+        public virtual int OnInviteEvent(rtInviteEvent e)
+        {
+            return 0;
+        }
+
+        public virtual int OnMessagingEvent(rtMessagingEvent e)
+        {
+            return 0;
+        }
+
+        public virtual int OnOptionsEvent(rtOptionsEvent e)
+        {
+            return 0;
+        }
+
+        public virtual int OnPublicationEvent(rtPublicationEvent e)
+        {
+            return 0;
+        }
+
+        public virtual int OnRegistrationEvent(rtRegistrationEvent e)
+        {
+            return 0;
+        }
+
+        public virtual int OnStackEvent(rtStackEvent e)
+        {
+            return 0;
+        }
+
+        public virtual int OnSubscriptionEvent(rtSubscriptionEvent e)
+        {
+            return 0;
+        }
+    }
+}
